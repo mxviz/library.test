e472b86a563021748c3639edfcdb2d8832dd5566
mxviz <vetal1423@gmail.com>
2022-04-08 12:34:45 +0300

    add relations to models Book, Author, Publisher

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+
+    public function authors()
+    {
+        return $this->belongsToMany(Author::class);
+    }
+
+    public function publishers()
+    {
+        return $this->belongsToMany(Publisher::class);
+    }
 }
