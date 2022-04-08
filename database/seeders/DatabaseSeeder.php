40796db2712ca5c685c9a85912e87230f5cceab8
mxviz <vetal1423@gmail.com>
2022-04-08 12:47:07 +0300

    add seeder condition and seed database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Publisher;
+use App\Models\Author;
+use App\Models\Book;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +17,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $publishers = Publisher::factory()->count(5)->create();
+        $authors = Author::factory()->count(10)->create();
+
+        Book::factory()->count(30)->create()->each(function($book) use ($publishers, $authors) {
+            $publishersId = $publishers->random(5)->pluck('id');
+            $authorsId = $authors->random(3)->pluck('id');
+
+            $book->publishers()->attach($publishersId);
+            $book->authors()->attach($authorsId);
+        });
     }
 }
