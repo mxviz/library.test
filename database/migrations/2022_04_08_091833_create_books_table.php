b67705da236cd35f69361967e6534d670b5c72c1
mxviz <vetal1423@gmail.com>
2022-04-08 12:22:19 +0300

    add fields to tables publishers, authors, books

--- database/migrations/2022_04_08_091833_create_books_table.php
+++ database/migrations/2022_04_08_091833_create_books_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
             $table->timestamps();
         });
     }
